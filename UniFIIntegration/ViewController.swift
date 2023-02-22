8c2acfc0f913590a3f269548d7165f02d240c99b
Varma <anjivarma.penmatsa@gmail.com>
2023-02-22 11:17:45 +0530

    second commit

--- UniFIIntegration/ViewController.swift
+++ UniFIIntegration/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     }()
     
     func makeD2DApiCall(){
-        guard let baseUrl = URL(string: "**")
+        guard let baseUrl = URL(string: "https://omni-apply.app.qa.pcf.syfbank.com/omniApply/application-link")
         else{
             return
         }
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
         let body: [String: Any] = [
             "waterfallFinance":"N",
             "applicantInfo":[
-                "emailAddress":"**",
+                "emailAddress":"penumatsa.anjaneyavarma@syf.com",
                 "saleAmount":"850",
                 "initialPurchaseAmount":850
             ],
@@ -40,16 +40,16 @@ class ViewController: UIViewController {
                 "productName":"ZETAIL VISA Rewards Card",
                 "channelId":"BC",
                 "productCodeGroupCode":"A600",
-                "merchantId":"**",
-                "storeMid":"**",
-                "partnerId":"**"
+                "merchantId":"5348120820046126",
+                "storeMid":"5348120820030393",
+                "partnerId":"PI1000011702"
             ],
             "strategyInfo":[
                 "viaCode":"H"
             ],
             "deliveryMethod":"EMAIL_AND_MERCHANT",
-            "d2dUrl":"**",
-            "applicationType":"**"
+            "d2dUrl":"https://qpdpone.syfpos.com/mppcore/d2d",
+            "applicationType":"MPP"
         ]
         print("body ==== \(body)")
         let requestBody = try! JSONSerialization.data(withJSONObject: body, options: [])
@@ -83,13 +83,15 @@ class ViewController: UIViewController {
     }
     
     @objc func tapWebView(){
-        guard let url = URL(string: "**")
+        guard let url = URL(string: "https://qpdpone.syfpos.com/mppcore/d2d/XNceLw7g")
         else{
             return
         }
         print("Hello world")
 
-        let jsonDictionary: [String: Any] = ["partnerId":"**", "amount":"1300", "pcgc":"GR10"]
+//        let jsonObject = "  {syfPartnerId:\"PI53421676\",tokenId:\"185df7a6fefPI5342167627741\",encryptKey:\"\",modalType:\"\",childMid:\"\",childPcgc:\"\",childTransType:\"\",pcgc:\"\",partnerCode:\"\",clientToken:\"\",postbackid:\"d979e5b7-6382-4e4e-b269-aab027bbed58\",clientTransId:\"\",cardNumber:\"\",custFirstName:\"\",custLastName:\"\",expMonth:\"\",expYear:\"\",custZipCode:\"\",custAddress1:\"\",phoneNumb:\"\",appartment:\"\",emailAddr:\"\",custCity:\"\",upeProgramName:\"\",custState:\"\",transPromo1:\"\",iniPurAmt:\"\",mid:\"\",productCategoryNames:\"\",transAmount1:\"700\",transAmounts:\"\",initialAmount:\"\",envUrl:\"https://dpdpone.syfpos.com/mitservice/\",productAttributes:\"\",processInd:\"3\"};"
+
+        let jsonDictionary: [String: Any] = ["partnerId":"P153421676", "amount":"1300", "pcgc":"GR10"]
 
         let json = try! JSONSerialization.data(withJSONObject: jsonDictionary, options: [])
 
--- UniFIIntegration/WebViewController.swift
+++ UniFIIntegration/WebViewController.swift
@@ -15,6 +15,10 @@ class WebViewController: UIViewController, WKNavigationDelegate, WKScriptMessage
     private let url: URL
     private let jsonData: String
     
+//    private let scriptSource = "var jsonObj =  {syfPartnerId:\"PI53421676\",tokenId:\"185df7a6fefPI5342167627741\",encryptKey:\"\",modalType:\"\",childMid:\"\",childPcgc:\"\",childTransType:\"\",pcgc:\"\",partnerCode:\"\",clientToken:\"\",postbackid:\"d979e5b7-6382-4e4e-b269-aab027bbed58\",clientTransId:\"\",cardNumber:\"\",custFirstName:\"\",custLastName:\"\",expMonth:\"\",expYear:\"\",custZipCode:\"\",custAddress1:\"\",phoneNumb:\"\",appartment:\"\",emailAddr:\"\",custCity:\"\",upeProgramName:\"\",custState:\"\",transPromo1:\"\",iniPurAmt:\"\",mid:\"\",productCategoryNames:\"\",transAmount1:\"700\",transAmounts:\"\",initialAmount:\"\",envUrl:\"https://dpdpone.syfpos.com/mitservice/\",productAttributes:\"\",processInd:\"3\"};"
+    
+//    lazy var jsonData: String = createJson(data: scriptSource)
+    
     init(url: URL, title: String, jsonData: String) {
         self.url = url
         self.jsonData = jsonData
@@ -54,10 +58,10 @@ class WebViewController: UIViewController, WKNavigationDelegate, WKScriptMessage
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         print("jsonData from the webview === \(jsonData)")
-//        webView.evaluateJavaScript("loadJsonData('\(jsonData)')") { (result, error) in
-//            dump(error)
-//            print(result)
-//        }
+        webView.evaluateJavaScript("loadJsonData('\(jsonData)')") { (result, error) in
+            dump(error)
+            print(result)
+        }
     }
                         
     
